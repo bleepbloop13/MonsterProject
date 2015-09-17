64ca1211e21300e478fa8e4e865681b2332ebff2
bleepbloop13 <rbak9218@740s-pl-107.local>
2015-09-17 12:42:36 -0600

    Created a method to allow user to create a monster instance

--- src/monster/controller/MonsterController.java
+++ src/monster/controller/MonsterController.java
@@ -8,7 +8,8 @@ public class MonsterController
 {
 
 	private Scanner monsterScanner;
-	private MonsterMash roryMonster;
+	private MonsterMash userMonster;
+	private MonsterMash roryMonster; //Declares a Monster called roryMonster.
 	private MonsterDisplay myDisplay;
 	
 	public MonsterController()
@@ -28,28 +29,66 @@ public class MonsterController
 	{
 		myDisplay.displayInfo(roryMonster.toString());
 		askQuestions();
-		myDisplay.displayInfo("Updated monster info: " + roryMonster.toString());
+		myDisplay.displayInfo(" Updated monster info: " + roryMonster.toString());
+		
+		myDisplay.displayInfo(userMonster.toString());
+		createUserMonster();
+		myDisplay.displayInfo(" Here's the info for your monster : " + userMonster.toString());
 	}
 	
 	private void askQuestions()
 	{
-		System.out.println("I want a new name for a monster, type one here please!");
+		System.out.println("I want a new name for my monster, type one here please!");
 		String newMonsterName = monsterScanner.next();
-		roryMonster.setMonsterName(newMonsterName);
+
 		System.out.println("Give me a new number of mouth pieces!!");
 		int newMonsterMouth = monsterScanner.nextInt();
-		roryMonster.setMonsterMouth(newMonsterMouth);
+		
 		System.out.println("Give me a cooler number of eyes!");
 		int newMonsterEyes = monsterScanner.nextInt();
-		roryMonster.setMonsterEyes(newMonsterEyes);
+		
 		System.out.println("Give me a new number of legs!");
 		double newMonsterLegs = monsterScanner.nextDouble();
-		roryMonster.setMonsterLegs(newMonsterLegs);
-		System.out.println("Give me a new numbe of hair strands!");
+		
+		System.out.println("Give me a new number of hair strands!");
 		double newMonsterHair = monsterScanner.nextDouble();
-		roryMonster.setMonsterHair(newMonsterHair);
+		
 		System.out.println("Do I have a belly button or not?");
 		boolean newMonsterBellyButton = monsterScanner.nextBoolean();
+		
+		roryMonster.setMonsterHair(newMonsterHair);
+		roryMonster.setMonsterLegs(newMonsterLegs);
+		roryMonster.setMonsterEyes(newMonsterEyes);
+		roryMonster.setMonsterMouth(newMonsterMouth);
+		roryMonster.setMonsterName(newMonsterName);
 		roryMonster.setMonsterBellyButton(newMonsterBellyButton);
+	}
+	
+	/**
+	 * Creates a MonsterMash instance from user input.
+	 */
+	private void createUserMonster()
+	{
+		//Step one: Gather user information
+		System.out.println("What is your monster's name?");
+		String userName = monsterScanner.nextLine();
+		
+		System.out.println("How many legs will it have? This is a decimal value!");
+		double userLegs = monsterScanner.nextDouble();
+		
+		System.out.println("How much hair will it have? Also a decimal!");
+		double userHair = monsterScanner.nextDouble();
+		
+		System.out.println("Does your monster have a belly button - true or false?");
+		boolean userHasBellyButton = monsterScanner.nextBoolean();
+		
+		System.out.println("How many mouth pieces does your monster have?");
+		int userMouths = monsterScanner.nextInt();
+		
+		System.out.println("How many eyes does you monster have?");
+		int userEyes = monsterScanner.nextInt();
+		
+		//Step two: Build the monster using the constructor.
+		userMonster = new MonsterMash(userName,userEyes, userMouths, userLegs, userHair, userHasBellyButton);
 	}
 }
